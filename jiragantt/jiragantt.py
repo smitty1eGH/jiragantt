580c1382866fa032424d55da7748630c867314e7
Chris Smith <smitty1e@gmail.com>
2020-08-29 08:54:22 -0400

    Working the org-mode end.

--- jiragantt/jiragantt.py
+++ jiragantt/jiragantt.py
@@ -1,12 +1,60 @@
+import csv
 import logging
 import logging.config
+import re
 
 logging.config.fileConfig("./etc/logging.conf", disable_existing_loggers=False)
 logger = logging.getLogger('jiragantt')
 
-def main() -> str :
-    logger.debug('Hello, world.')
-    return 'Hello, world.'
+NON_CHARS='\W*'
 
-if __name__=='__main__':
-    print(main())
+#Field names retrieve directly from the Jira .csv. This list is like an SQL
+#  SELECT clause for the  fj
+FIELDS=['Summary'                      ,'Issue key'                  ,'Assignee'
+       ,'Created'                      ,'Due Date'                   ,'Description'
+       ,'Original Estimate'            ,'Remaining Estimate'         ,'Time Spent'
+       ,'Outward issue link (Precedes)','Custom field (Epic Link)'   ,'Custom field (Epic Name)'
+       ,'Custom field (Rank)'          ,'Custom field (Story Points)'
+       ]
+#This is the form to return for dumping the Jira item to a straight LaTeX file
+LATEX_REPR='''#+LATEX_HEADER: \\usepackage{pgfgantt}
+ALL_YOUR_JIRA_ITEMS
+#+BEGIN: org-gantt-chart :id "todo-deadlines-schedules"
+#+END
+'''
+
+#This is the form to return for dumping the Jira item to a .org agenda file
+#  Need to group on Epic Name with **
+#  And then put out the Summary with ***
+ORG_AGENDA=''' '''
+
+class JiraGantt:
+    '''Set up a specific .org file for having the Jira data witten as a Gantt.
+    '''
+    def __init__(self,outfile):
+        self.outfile=outfile
+
+    def add_jira_item(self,jira_item):
+        pass
+
+    def __str__(self):
+        pass
+
+
+class JiraItem:
+    def __init__(self,jira_fields,jira_item):
+
+        def tidy_field(field):
+            return re.sub(NON_CHARS,'',field)
+
+        self.tidy_fields={}
+        for jf in jira_fields:
+            tf = tidy_field(jf)
+            self.tidy_fields[tidy_field(tf)] = jf
+            setattr(self, tf, jira_item[jf] )
+
+    def __repr__(self):
+        return f'{{"Summary":"{self.Summary}"}}'
+
+    def __str__(self):
+        return f'The name is {self.Summary}'
--- tests/test_jiragantt.py
+++ tests/test_jiragantt.py
@@ -1,6 +1,19 @@
+import csv
+
 import pytest
 
-from jiragantt.jiragantt import main
+from jiragantt.jiragantt import FIELDS, JiraItem
 
-def test_main():
-    assert main() == "Hello, world."
+@pytest.fixture
+def data_spec():
+    return '/mnt/osis/jira.csv'
+
+def test_main(data_spec):
+    jira_items=[]
+    with open(data_spec,'r') as csvf:
+        tasks=csv.DictReader(csvf)
+        for t in tasks:
+            jira_items.append(JiraItem(FIELDS,t))
+    jira_items.sort(key=lambda x: x.CustomfieldRank)
+    print(jira_items[0].__repr__())
+    print(jira_items[0])
