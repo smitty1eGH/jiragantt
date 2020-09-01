85ee19c6ffc8451fbd9fa1b92822fa55b0ef06b4
Chris Smith <smitty1e@gmail.com>
2020-09-01 10:30:46 -0400

    WIP

--- jiragantt/jiragantt.py
+++ jiragantt/jiragantt.py
@@ -4,57 +4,123 @@ import logging.config
 import re
 
 logging.config.fileConfig("./etc/logging.conf", disable_existing_loggers=False)
-logger = logging.getLogger('jiragantt')
+logger = logging.getLogger("jiragantt")
 
-NON_CHARS='\W*'
+NON_CHARS = "\W*"
 
-#Field names retrieve directly from the Jira .csv. This list is like an SQL
+# Field names retrieve directly from the Jira .csv. This list is like an SQL
 #  SELECT clause for the  fj
-FIELDS=['Summary'                      ,'Issue key'                  ,'Assignee'
-       ,'Created'                      ,'Due Date'                   ,'Description'
-       ,'Original Estimate'            ,'Remaining Estimate'         ,'Time Spent'
-       ,'Outward issue link (Precedes)','Custom field (Epic Link)'   ,'Custom field (Epic Name)'
-       ,'Custom field (Rank)'          ,'Custom field (Story Points)'
-       ]
-#This is the form to return for dumping the Jira item to a straight LaTeX file
-LATEX_REPR='''#+LATEX_HEADER: \\usepackage{pgfgantt}
+FIELDS = [
+    "Summary",
+    "Issue key",
+    "Assignee",
+    "Created",
+    "Due Date",
+    "Description",
+    "Original Estimate",
+    "Remaining Estimate",
+    "Time Spent",
+    "Outward issue link (Precedes)",
+    "Custom field (Epic Link)",
+    "Custom field (Epic Name)",
+    "Custom field (Rank)",
+    "Custom field (Story Points)",
+]
+
+# Name of generic epic
+CATCH_ALL = "catch_all"
+
+# This is the form to return for dumping the Jira item to a straight LaTeX file
+LATEX_REPR = """#+LATEX_HEADER: \\usepackage{pgfgantt}
 ALL_YOUR_JIRA_ITEMS
 #+BEGIN: org-gantt-chart :id "todo-deadlines-schedules"
 #+END
-'''
+"""
 
-#This is the form to return for dumping the Jira item to a .org agenda file
+# This is the form to return for dumping the Jira item to a .org agenda file
 #  Need to group on Epic Name with **
 #  And then put out the Summary with ***
-ORG_AGENDA=''' '''
+ORG_AGENDA = """ """
+
 
 class JiraGantt:
-    '''Set up a specific .org file for having the Jira data witten as a Gantt.
-    '''
-    def __init__(self,outfile):
-        self.outfile=outfile
+    """Set up a specific .org file for having the Jira data witten as a Gantt.
+    """
 
-    def add_jira_item(self,jira_item):
-        pass
+    def __init__(self, out_file):
+        self.out_file = out_file
+        self.jira_epics = set()
+        self.jira_items = {}
+        self.epics2items = set()
 
-    def __str__(self):
-        pass
+    def add_jira_item(self, jira_item):
+        ji = JiraItem(FIELDS, jira_item)
+        ji_epic = ji.epic_name
+        ji_rank = ji.jira_rank
+        if ji.epic_name not in self.jira_epics:
+            self.jira_epics.add(ji.epic_name)
+        self.epics2items.add((ji_epic, ji_rank))
+        self.jira_items[ji_rank] = ji
+
+    def all_your_jira_items(self):
+        '''Roll up the list of jira items do as a list comprehension, maybe make a generator
+        '''
+        output = []
+        for epic in self.jira_epics:
+
+            output.append(f'** {epic}')
+
+            items = [item for item in self.epics2items if item[0] == epic]
+
+            for item in items:
+                output.append(item.__str__())
+
+        return '\n'.join(output)
+
+    def publish(self):
+        """Output a .org file suitable to ganttify
+        """
+        with open(self.out_file,'w') as f:
+            f.write(LATEX_REPR.replace('ALL_YOUR_JIRA_ITEMS',self.all_your_jira_items()))
+
 
 
 class JiraItem:
-    def __init__(self,jira_fields,jira_item):
-
-        def tidy_field(field):
-            return re.sub(NON_CHARS,'',field)
-
-        self.tidy_fields={}
+    def __init__(self, jira_fields, jira_item):
+        self.tidy_fields = {}
         for jf in jira_fields:
-            tf = tidy_field(jf)
-            self.tidy_fields[tidy_field(tf)] = jf
-            setattr(self, tf, jira_item[jf] )
+            tf = self.tidy_field(jf)
+            self.tidy_fields[self.tidy_field(tf)] = jf
+            setattr(self, tf, jira_item[jf])
+
+    def tidy_field(self,field):
+        return re.sub(NON_CHARS, "", field)
+
+    @property
+    def epic_name(self):
+        return getattr(self, self.tidy_fields[self.tidy_field("Custom field (Epic Name)")], CATCH_ALL)
+
+    @property
+    def jira_rank(self):
+        return getattr(self, self.tidy_fields[self.tidy_field("Custom field (Rank)")],'0')
 
     def __repr__(self):
+        '''Put out a straight pgfgantt representation of the item.
+        '''
         return f'{{"Summary":"{self.Summary}"}}'
 
     def __str__(self):
-        return f'The name is {self.Summary}'
+        '''Put out an org-gantt ready version
+        '''
+        return f"*** {self.Summary}"
+
+def jira_csv_to_org(data_spec, out_file):
+    '''Transform the file DATA_SPEC to OUT_FILE
+
+    '''
+    jg = JiraGantt(out_file)
+    with open(data_spec, "r") as csvf:
+        tasks = csv.DictReader(csvf)
+        for t in tasks:
+            jg.add_jira_item(t)
+    jg.publish()
--- tests/test_jiragantt.py
+++ tests/test_jiragantt.py
@@ -2,18 +2,26 @@ import csv
 
 import pytest
 
-from jiragantt.jiragantt import FIELDS, JiraItem
+from jiragantt.jiragantt import FIELDS, JiraItem, JiraGantt,jira_csv_to_org
+
 
 @pytest.fixture
 def data_spec():
-    return '/mnt/osis/jira.csv'
+    return "/mnt/osis/jira.csv"
 
-def test_main(data_spec):
-    jira_items=[]
-    with open(data_spec,'r') as csvf:
-        tasks=csv.DictReader(csvf)
+@pytest.fixture
+def out_file(tmp_path):
+    return f'{tmp_path}/asdf.org'
+
+def test_jira_item(data_spec):
+    jira_items = []
+    with open(data_spec, "r") as csvf:
+        tasks = csv.DictReader(csvf)
         for t in tasks:
-            jira_items.append(JiraItem(FIELDS,t))
+            jira_items.append(JiraItem(FIELDS, t))
     jira_items.sort(key=lambda x: x.CustomfieldRank)
     print(jira_items[0].__repr__())
     print(jira_items[0])
+
+def test_jira_csv_to_org(data_spec, out_file):
+    jira_csv_to_org(data_spec, out_file)
